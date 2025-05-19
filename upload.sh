79a7effa4d8d695f4068725446ec115590e16b16
LucasJSch <lucas.scheinkerman@gmail.com>
2025-05-19 13:15:31 -0300

    Deleted upload

--- upload.sh
+++
@@ -1,6 +0,0 @@
-export GITHUB_USER=USER
-export GITHUB_TOKEN=TOKEN
-git add .
-git commit -m "Update."
-git push https://$GITHUB_USER:$GITHUB_TOKEN@github.com/lucasjsch/limud-resources.git gh-pages
-git pull origin gh-pages
